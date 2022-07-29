48c43d271ee8f23db14897a1d2477ed87f38647e
John Perez <55162758+jjperez9000@users.noreply.github.com>
2022-07-29 18:31:31 -0400

    Initial Commit

---
+++ project_theme/project_themeApp.swift
@@ -0,0 +1,20 @@
+//
+//  project_themeApp.swift
+//  project_theme
+//
+//  Created by John Perez on 7/29/22.
+//
+
+import SwiftUI
+
+@main
+struct project_themeApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
